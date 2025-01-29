fd483402b04294a70bd17a49d213b466e7c5740e
Andrea Morales Villegas <moralesa7412@gmail.com>
2025-01-29 10:28:34 -0800

    new enemy ship is now added, speed change needed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,6 +23,9 @@ class Play extends Phaser.Scene {
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0)
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0)
 
+        // add smaller enemy spaceship
+        this.ship04 = new Spaceship(this, game.config.width, borderUISize*3 + borderPadding*5, 'enemyship', 0, 0).setOrigin(0, 0)
+
         // define keys
         keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
         keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
@@ -76,9 +79,14 @@ class Play extends Phaser.Scene {
             this.ship01.update() // update the spaceships (x3)
             this.ship02.update()
             this.ship03.update()
+            this.ship04.update()
         }
 
         // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship04)) {
+            this.p1Rocket.reset()
+            this.shipExplode(this.ship04)
+        }
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset()
             this.shipExplode(this.ship03)
